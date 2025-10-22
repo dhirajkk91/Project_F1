95a9444ddd0035c2a89fbffac66e7a1a38e0133b
Dhiraj Karki <dhirajkk91@email.com>
2025-10-21 21:48:34 -0500

    making a loop that will feed the inputs to result.py

--- DATA/Data Processing/Main.py
+++ DATA/Data Processing/Main.py
@@ -1,18 +1,16 @@
 
 import fastf1
-from Results import get_result
+from result import get_result
 
-print("Tell me the race you want to know about")
+fastf1.Cache.enable_cache('DATA/Data%20Processing/_pycache_')
 
-date = input("Race year: ")
-date_int = int(date)  # Convert string to integer
-if(date_int > 1990 and date_int <= 2025):
-    print("Date is good")
-    date = str(date_int)
-else:
-    print("error")
+Year = [2025 ,2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017]
+
+for year in Year:
+    schedule = fastf1.get_event_schedule(year)
+    Name = [schedule["EventName"].unique()]
     
-race_name = input("Race name: ")
-race_type = input("Select, FP1, FP2, Q, R: ")
+    for name in Name[0]:
+        print(f"Getting results for {name} {year}...")
+        get_result(str(year), name, "R")
 
-print(get_result(date, race_name, race_type))
